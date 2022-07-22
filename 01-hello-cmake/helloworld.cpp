7e165ad21b6b3e915dc20191b3612ab77ef369a7
Алексей Сычёв <seechov@gmail.com>
2022-07-22 17:57:38 +0300

    astyle applied

--- 01-hello-cmake/helloworld.cpp
+++ 01-hello-cmake/helloworld.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 #include <string>
 
-std::string say_hello() { return std::string("Hello, CMake world!"); }
+std::string say_hello()
+{
+    return std::string("Hello, CMake world!");
+}
 
-int main() {
-  std::cout << say_hello() << std::endl;
-  return EXIT_SUCCESS;
+int main()
+{
+    std::cout << say_hello() << std::endl;
+    return EXIT_SUCCESS;
 }
