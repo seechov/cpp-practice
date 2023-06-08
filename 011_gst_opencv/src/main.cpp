7ced8fd3bd6e5fcd7b1035683389f8da3a018e44
seechov <98948631+seechov@users.noreply.github.com>
2023-06-08 12:53:46 +0300

    Fix for "broken stride".

--- 011_gst_opencv/src/main.cpp
+++ 011_gst_opencv/src/main.cpp
@@ -15,10 +15,11 @@ int main(int argc, char** argv) {
 		printf("ERROR! Unable to open camera\n");
 		return -1;
 	}
+	int codec = cv::VideoWriter::fourcc('x', '2', '6', '4');  // select desired codec (must be available at runtime)
 	cv::VideoWriter writer;
 	writer.open("appsrc ! videoconvert ! x264enc noise-reduction=10000 tune=zerolatency byte-stream=true threads=4 ! mpegtsmux ! udpsink host=10.1.2.107 port=5000",
 		cv::CAP_GSTREAMER,
-		0,
+		codec,
 		16,
 		cv::Size(640, 480),
 		true);
@@ -27,9 +28,11 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+	int i = 0;
 	printf("Start grabbing\n");
 	while (true)
 	{
+
 		// wait for a new frame from camera and store it into 'frame'
 		cap.read(frame);
 		// check if we succeeded
@@ -43,12 +46,15 @@ int main(int argc, char** argv) {
 		time_str[strlen(time_str) - 1] = '\0';
 		printf("[%s] Frame was captured.\n", time_str);
 
-		putText(frame, time_str, cv::Point(5, 5),
+		cv::Mat frame_resized;
+		resize(frame, frame_resized, cv::Size(640, 480), 0, 0, cv::INTER_CUBIC);
+
+		putText(frame_resized, time_str, cv::Point(20, 20),
 			cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, cv::Scalar(200, 200, 250), 1, cv::LINE_AA);
-		// show live and wait for a key with timeout long enough to show images
-		writer.write(frame);
 
-
+		writer.write(frame_resized);
+		cv::String filename = cv::format("frame_%d.jpg", i++);
+		// cv::imwrite(filename, frame);
 	}
 	// the camera will be deinitialized automatically in VideoCapture destructor
 	return 0;
